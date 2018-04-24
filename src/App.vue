60d8c30287048d61213be9c9476686dffeb70a27
Levi <levimcg@gmail.com>
2018-04-24 08:49:45 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="rvt-container rvt-m-top-xl">
+        <Tabs label="Vue tabs">
+            <TabsPanel name="About us" :selected="true">
+                About panel
+            </TabsPanel>
+            <TabsPanel name="Team">
+                Team panel
+            </TabsPanel>
+            <TabsPanel name="Contact">
+                Contact panel
+            </TabsPanel>
+            <TabsPanel name="Schedule">
+                Schedule panel
+            </TabsPanel>
+        </Tabs>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Tabs from './components/Tabs';
+    import TabsPanel from './components/TabsPanel';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Tabs, TabsPanel
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    html * {
+        box-sizing: border-box;
+    }
+
+    #app {
+        padding: 3rem;
+    }
 </style>
